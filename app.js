dff7eaa6d74571db6a561e2217a86d9b6b695709
UtKaRsh7K <utkarshk2207@gmail.com>
2023-02-05 20:44:01 +0530

    Letting users Submit Secrets

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ const userSchema = new mongoose.Schema({
   password: String,
   googleId: String,
   facebookId: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -51,8 +52,10 @@ passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 passport.use(
@@ -89,7 +92,7 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-//! Routes for Google Authentication
+//* Routes for Google Authentication
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
@@ -104,7 +107,7 @@ app.get(
   }
 );
 
-//! Routes for Facebook Authentication
+//* Routes for Facebook Authentication
 app.get("/auth/facebook", passport.authenticate("facebook"));
 
 app.get(
@@ -126,11 +129,40 @@ app.get("/register", function (req, res) {
 
 // If a autheticated user want to access direct secrets page.
 app.get("/secrets", function (req, res) {
+  User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", { usersWithSecrets: foundUsers });
+      }
+    }
+  });
+});
+
+app.get("/submit", function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function (req, res) {
